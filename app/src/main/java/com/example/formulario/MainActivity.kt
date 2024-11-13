83ae5a170f39b601a03f2dd0732bf803ceac2262
NICOLASUPB <caldenico2008@gmail.com>
2024-11-13 17:20:35 -0500

    commit desde codigo tercer commit

--- app/src/main/java/com/example/formulario/MainActivity.kt
+++ app/src/main/java/com/example/formulario/MainActivity.kt
@@ -7,5 +7,30 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+ASJDFALSJDCLKSAJNVOÑDVD
+    SVDSMCVKSdhicfdjskjfvdn
+    vnsadkvsdhfvdlwnd
+    vcdhws  vjdwshvdfvc
+    cjd<zkCVUDSpovw
+     dhnviedockvpskmcf
+    dfg
+    fdashfalñf
+    afjaf
+    JF
+    jf
+    0EFUJEif
+    FwhfE´FHqe
+    gfhA
+    FGH
+    ehgf
+    HGE'
+    Wg
+    EHF
+    'wegfh
+    EEG
+    wehg
+    WEG
+    efhe'fg
 
+    '
 }
